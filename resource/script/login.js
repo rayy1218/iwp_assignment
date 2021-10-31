a45d74bd29af604e89ae5738cca70ba2f591cb6e
ray1218 <raytham1218@gmail.com>
2021-10-31 16:38:54 +0800

    setItemDisplay function

--- resource/script/login.js
+++ resource/script/login.js
@@ -40,6 +40,13 @@ function initLogin() {
         setUsername("null");
     }
 
+    setItemDisplay();
+
+    let user_tab = document.getElementById("debug-user-tab");
+    user_tab.innerHTML = `Status: ${getLoginStatusString(getLoginStatus())} Username: ${getUsername()}`;
+}
+
+function setItemDisplay() {
     let user_item = document.getElementsByClassName("user"),
         non_user_item = document.getElementsByClassName("non-user");
 
@@ -61,9 +68,6 @@ function initLogin() {
             non_user_item[i].style.display = "block";
         }
     }
-
-    let user_tab = document.getElementById("debug-user-tab");
-    user_tab.innerHTML = `Status: ${getLoginStatusString(getLoginStatus())} Username: ${getUsername()}`;
 }
 
 function doRegister() {
